8687977d58a89bd1fccb66d2f34fb561cfbf8203
MArslan88 <gery_maz_555@hotmail.com>
2022-07-16 08:33:53 +0500

    var and val

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+    /*
+    var (mutable: value can be reassign)
+    val (immutable: value can not be reassign)
+     */
+    var name = "Arslan"     // mutable
+    val name2 = "Danish"    // immutable
+    name = "Tanzeel"
+
+    println(name)
+    println(name2)
+
+
+
+}
